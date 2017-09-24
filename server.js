95f4acfd3e9f5d1d29b16912fb0a025ad550d166
TheRealMPC <smkscar25@hotmail.com>
2017-09-24 02:13:05 -0500

    sql assessment complete

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
 const express = require('express')
-    , bodyParser = require('body-parser')
+    , { json } = require('body-parser')
     , cors = require('cors')
-    , massive = require('massive');
+    , PORT = 3000
+    , massive = require('massive')
+    , mainCtrl = require('./mainCtrl')
+    , config = require('./config')
+    , app = express();
 
-const mainCtrl = require('./mainCtrl');
 
-const app = express();
-
-app.use(bodyParser.json())
+app.use(json());
 app.use(cors());
 
 // You need to complete the information below to connect
 // to the assessbox database on your postgres server.
-massive({
-  host: //host,
-  port: //port,
-  database: //database,
-  user: //user,
-  password: //password
-}).then( db => {
-  app.set('db', db);
+massive(config.massiveConnectionString).then(dbInstance => {
+  app.set("db", dbInstance);
 
   // Initialize user table and vehicle table.
-  db.init_tables.user_create_seed().then( response => {
-    console.log('User table init');
-    db.init_tables.vehicle_create_seed().then( response => {
-      console.log('Vehicle table init');
-    })
+  dbInstance.init_tables.user_create_seed().then(response => {
+      console.log('User table init');
+      dbInstance.init_tables.vehicle_create_seed().then(response => {
+          console.log('Vehicle table init');
+      })
   })
 
 })
 
-
 // ===== Build enpoints below ============
+app.get('/api/users', mainCtrl.getAllUsers);
+app.get('/api/vehicles', mainCtrl.getAllVehicles);
+app.get('/api/user/:id/vehiclecount', mainCtrl.getCountCarsByID);
+app.get('/api/user/:id/vehicle', mainCtrl.getCarsByUserID);
+app.get('/api/newervehiclesbyyear', mainCtrl.getVehiclesByYear);
+app.get('/api/vehicle', mainCtrl.getQuery);
+
+app.put('/api/vehicle/:vid/user/:uid', mainCtrl.updateVehicleOwner);
 
 
+app.post('/api/users', mainCtrl.addUser);
+app.post('/api/vehicles', mainCtrl.addVehicle);
 
-
-
-
+app.delete('/api/user/:uid/vehicle/:vid', mainCtrl.deleteOwnership);
+app.delete('/api/vehicle/:id', mainCtrl.deleteVehicleByID);
 
 // ===== Do not change port ===============
-const port = 3000;
-app.listen(port, () => {
-  console.log('Listening on port: ', port);
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
 })
